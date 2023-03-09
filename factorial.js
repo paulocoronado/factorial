4c5c71f750ba0fc39bea5c917bd5ef172e3b5fc0
Paulo Coronado <paulocoronado@udistrital.edu.co>
2023-03-09 07:31:05 -0500

    v 2.0

---
+++ factorial.js
@@ -0,0 +1,12 @@
+
+function factorial(){
+
+    let n= parseInt(document.getElementById('n').value);
+    let factorial=1;
+    let i=2;
+    while(i<=n){
+        factorial=factorial*i;
+        i++;
+    }
+    return factorial;
+}
